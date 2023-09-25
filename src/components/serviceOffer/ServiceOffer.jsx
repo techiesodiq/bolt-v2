6bb1ddc5e45f4caffb068f5a35c5caff09a108fb
Anuoluwa <hernuincredible@gmail.com>
2023-09-25 07:41:19 +0100

    service

--- src/components/serviceOffer/ServiceOffer.jsx
+++ src/components/serviceOffer/ServiceOffer.jsx
@@ -10,16 +10,33 @@ import {
   SolutionCard,
   Title,
 } from "./serviceOffer.style";
+import { motion } from "framer-motion";
 
 const { title, options } = services;
 
 const ServiceOffer = () => {
   return (
     <ServiceBox>
-      <Title>{title}</Title>
+      <Title
+        as={motion.h2}
+        initial={{ y: "-150%", opacity: 0 }}
+        animate={{ y: 0, opacity: 1, transition: { duration: 1 } }}
+      >
+        {title}
+      </Title>
       <SolutionBox>
         {options.map((option, index) => (
-          <SolutionCard key={index}>
+          <SolutionCard
+            key={index}
+            as={motion.div}
+            initial={{ x: "-100%", opacity: 0 }}
+            whileInView={{
+              x: 0,
+              opacity: 1,
+              transition: { duration: 0.8, delay: index % 2 === 0 ? 0 : 1 },
+            }}
+            viewport={{ once: true }}
+          >
             <div>
               <CardTitle>{option.title}</CardTitle>
               <CardParagraph>{option.description}</CardParagraph>
