154dcc1d98dd98d364f5013917f401d2f4407f2f
Anuoluwa <hernuincredible@gmail.com>
2023-09-25 08:24:39 +0100

    career

--- src/components/career/Career.jsx
+++ src/components/career/Career.jsx
@@ -14,6 +14,7 @@ import {
   Title,
 } from "./career.style";
 import { career } from "../../data/text";
+import { motion } from "framer-motion";
 
 const { description, img, options, subTitle, title } = career;
 
@@ -21,20 +22,74 @@ const Career = () => {
   return (
     <>
       <CareerHeader>
-        <Title>{title}</Title>
-        <Paragraph>{description}</Paragraph>
+        <Title
+          as={motion.h2}
+          initial={{ y: "-100%", opacity: 0 }}
+          whileInView={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 1 },
+          }}
+          viewport={{ once: true }}
+        >
+          {title}
+        </Title>
+        <Paragraph
+          as={motion.h2}
+          initial={{ y: "100%", opacity: 0 }}
+          whileInView={{
+            y: 0,
+            opacity: 1,
+            transition: { duration: 1 },
+          }}
+          viewport={{ once: true }}
+        >
+          {description}
+        </Paragraph>
       </CareerHeader>
       <ImgWrapper>
         {img.map((item, index) => (
-          <Image src={item} alt="fun" />
+          <Image
+            src={item}
+            alt="fun"
+            as={motion.img}
+            initial={{ y: -100, opacity: 0 }}
+            whileInView={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 0.9 },
+            }}
+            viewport={{ once: true }}
+          />
         ))}
       </ImgWrapper>
-      <SubTitle>{subTitle}</SubTitle>
+      <SubTitle
+        as={motion.h2}
+        initial={{ y: "-100%", opacity: 0 }}
+        whileInView={{
+          y: 0,
+          opacity: 1,
+          transition: { duration: 1 },
+        }}
+        viewport={{ once: true }}
+      >
+        {subTitle}
+      </SubTitle>
 
       <MarginBottom>
         {options.map((option, index) => (
-          <JobBox>
-            <TextField key={index} to="/career/requirement">
+          <JobBox
+            as={motion.div}
+            key={index}
+            initial={{ y: -100, opacity: 0 }}
+            whileInView={{
+              y: 0,
+              opacity: 1,
+              transition: { duration: 1, delay: 0.9 },
+            }}
+            viewport={{ once: true }}
+          >
+            <TextField to="/career/requirement">
               <JobTitle>{option.title}</JobTitle>
               <JobText>{option.description}</JobText>
             </TextField>
